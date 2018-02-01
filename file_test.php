3da74e747f1ca8c22d7099dccb2f38e2ee0a2b58
Ishti <ia26993@gmail.com>
2018-02-01 15:03:59 +0600

    Update file_test.php

--- file_test.php
+++ file_test.php
@@ -7,7 +7,8 @@ function write_log($text_to_write)
 	$text_to_write.=$text;
 	$success=file_put_contents('log.txt', $text_to_write);
 }
-
+// file_put_contents('logs.txt', $text_to_write.PHP_EOL , FILE_APPEND | LOCK_EX);
+//to append to existing file the command above should be written
 for($x=0;$x<10;$x++)
 {
 	write_log($x);
